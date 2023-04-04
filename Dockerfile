d415de4efce428b772e4d072600e570a3da3bd1a
fredhcr <frederycastillo21@gmail.com>
2023-04-04 14:33:54 -0500

    Connection Google Cloud Run

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM node:lts-hydrogen AS base
+WORKDIR /root/app
+COPY package.json package-lock.json ./
+
+FROM base AS dependencies
+RUN npm set progress=false && npm config set depth 0
+RUN npm install --only=production
+RUN cp -R node_modules /tmp/prod_node_modules
+RUN npm install
+COPY . .
+
+FROM dependencies AS build
+RUN npm run build
+RUN cp -R dist /tmp/prod_dist
+
+FROM base AS release
+COPY --from=dependencies /tmp/prod_node_modules ./node_modules
+COPY --from=build /tmp/prod_dist .
+RUN npm install nodemon pino-pretty -g
+EXPOSE 8080
+CMD [ "node", "./src/index.js" ]
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,14 @@ import { AppService } from './app.service';
 import { SeaPortsModule } from './api/modules/sea-ports/sea-ports.module';
 import { BoatsModule } from './api/modules/boats/boats.module';
 import { ReservationsModule } from './api/modules/reservations/reservations.module';
-// import dataBaseConfig from './config/db.interface';
+import dataBaseConfig from './config/db.interface';
 // const userPass =
 //   dataBaseConfig.mongoUser && dataBaseConfig.mongoPassword
 //     ? `${dataBaseConfig.mongoUser}:${dataBaseConfig.mongoPassword}@`
 //     : '';
 @Module({
   imports: [
-    MongooseModule.forRoot(`mongodb://localhost/db`),
+    MongooseModule.forRoot(dataBaseConfig.mongoUri ?? `mongodb://localhost/db`),
     SeaPortsModule,
     BoatsModule,
     ReservationsModule,
